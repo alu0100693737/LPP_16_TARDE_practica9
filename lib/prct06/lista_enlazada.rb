41242ce3b5d8d39dcce89e9429e394ca6f920f31
Ivan Garcia <alu0100693737@ull.edu.es>
2014-10-29 18:36:42 +0100

    empezando a generar la clase lista

---
+++ lib/prct06/lista_enlazada.rb
@@ -0,0 +1,19 @@
+module Prct06
+  class Lista_enlazada
+    
+    attr_accessor :cabeza, :ultelem
+    
+    # create a Struct with :value and :next
+    
+    def initialize(cabeza = nil, ultelem = nil)
+    end
+    
+    def push(args)
+      array=[]
+      array.unshift << args
+    end 
+    
+    
+  end
+end
+    
