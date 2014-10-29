102939ab2b770b6677d288b97ed8fd4579400253
Fabián Díaz Lorenzo <alu0100702545@ull.edu.es>
2014-10-29 18:30:29 +0000

    implementando las primeras partes de los test

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -2,5 +2,31 @@ require 'spec_helper'
 require 'prct06'
 
 describe Prct06 do
-  it 'requires additional testing'
+	class Lista_enlazada
+		describe Prct06::Lista_enlazada do
+
+			before :each do
+				Nodo = Struct.new(:valor, :siguiente)
+				@preg1=Preguntas::EleccionSimple.new(
+					:pregunta => "salida de : 
+					class Xyz \n def pots\n
+					@nice\n end\nend\n",:Op_correcta => 'nil',
+				 	:Op_incorrecta['#<Xyz:0x00000002bf0ed0>',0, "ninguna de las anteriores" )
+				@nodo1=Nodo.new(@preg1, nil)
+				@lista_enlazada=Prct06::Lista_enlazada.new(:cabeza=>nil,:ultelem =>nil)
+				@lista_enlazada.push(@nodo1)	
+			end
+
+
+
+
+
+		end 
+		
+	
+
+	end
+
+
+ 
 end
