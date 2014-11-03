b3feec15f962b30c2b5e2622358a18525c2c355a
Ivan Garcia <alu0100693737@ull.edu.es>
2014-11-03 23:05:25 +0100

    Test sin funcionar añadidos

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -8,37 +8,37 @@ describe Prct06 do
 			before :each do
 				nodo = Struct.new(:valor, :siguiente)
 				@lista_enlazada2=Prct06::Lista_enlazada.new
- 				@preg1=Preguntas::EleccionSimple.new(
- 					:pregunta => "salida de : 
- 					class Xyz \n def pots\n
- 					@nice\n end\nend\n",:Op_correcta => 'nil',
- 				 	:Op_incorrecta => ['#<Xyz:0x00000002bf0ed0>',0, "ninguna de las anteriores" ])
- 
- 				@nodo1=nodo.new(@preg1, nil)
- 				@lista_enlazada=Prct06::Lista_enlazada.new
- 				
- 				@preg2=Preguntas::EleccionSimple.new(
- 				      :pregunta => "salida de :
- 				      hash_raro ={ \n
- 				      [1, 2, 3] => Object.new(), \n
- 				      Hash.new => :toto
- 				      }", :Op_correcta => "Cierto", 
- 				      :Op_incorrecta => "Falso" )
- 				
- 				@nodo2=nodo.new(@preg2, nil)
- 				
- 				@preg3=Preguntas::EleccionSimple.new(
- 				      :pregunta => "salida de :
- 				      class Array \n 
- 				      def say_hi \n
- 				      HEY! 
- 				      end
- 				      end \n
- 				      p[1,, bob].say_hi", 
- 				      :Op_correcta => "Ninguna de las anteriores",
- 				      :Op_incorrecta => ['1', 'bob', 'HEY!'])
- 				      
- 				@nodo3=nodo.new(@preg3, nil)
+#  				@preg1=Preguntas::EleccionSimple.new(
+#  					:pregunta => "salida de : 
+#  					class Xyz \n def pots\n
+#  					@nice\n end\nend\n",:Op_correcta => 'nil',
+#  				 	:Op_incorrecta => ['#<Xyz:0x00000002bf0ed0>',0, "ninguna de las anteriores" ])
+#  
+#  				@nodo1=nodo.new(@preg1, nil)
+#  				@lista_enlazada=Prct06::Lista_enlazada.new
+#  				
+#  				@preg2=Preguntas::EleccionSimple.new(
+#  				      :pregunta => "salida de :
+#  				      hash_raro ={ \n
+#  				      [1, 2, 3] => Object.new(), \n
+#  				      Hash.new => :toto
+#  				      }", :Op_correcta => "Cierto", 
+#  				      :Op_incorrecta => "Falso" )
+#  				
+#  				@nodo2=nodo.new(@preg2, nil)
+#  				
+#  				@preg3=Preguntas::EleccionSimple.new(
+#  				      :pregunta => "salida de :
+#  				      class Array \n 
+#  				      def say_hi \n
+#  				      HEY! 
+#  				      end
+#  				      end \n
+#  				      p[1,, bob].say_hi", 
+#  				      :Op_correcta => "Ninguna de las anteriores",
+#  				      :Op_incorrecta => ['1', 'bob', 'HEY!'])
+#  				      
+#  				@nodo3=nodo.new(@preg3, nil)
  				
  				@preg4=Preguntas::EleccionSimple.new(
  				      :pregunta => "salida de :
@@ -46,26 +46,43 @@ describe Prct06 do
  				      end", :Op_correcta => "Una instancia de la clase Class",
  				      :Op_incorrecta => ['una constante', 'un objeto', 'ninguna de las anteriores'])
  				
- 				@nodo4=nodo.new(@preg4, nil)
+ 				
  				
  				@preg5=Preguntas::EleccionSimple.new(
  				      :pregunta => "Es apropiado que una clase tablero herede de la clase juego? ",
  				      :Op_correcta => "Falso", :Op_incorrecta => ['Cierto'])
- 				
+				
+ 				@nodo4=nodo.new(@preg4, nil)
  				@nodo5=nodo.new(@preg5, nil)
- 				
- 				
- 				@lista_enlazada.push(@nodo1)
- 				@lista_enlazada.push(@nodo2)
- 				@lista_enlazada.push(@nodo3)
+ 						
+#  				@lista_enlazada.push(@nodo1)
+#  				@lista_enlazada.push(@nodo2)
+#  				@lista_enlazada.push(@nodo3)
  				@lista_enlazada.push(@nodo4)
  				@lista_enlazada.push(@nodo5)
-				
 				@lista_enlazada.pop
 				
 				@nodo6=nodo.new("2", nil)
  				@lista_enlazada2.push(@nodo6)
  			end
+			
+			context "cuando se construye una pregunta" do
+				it " tiene que tener un texto y algunas opciones" do
+					expect(@preg4.pregunta).to match("salida de ")
+					expect(@preg4.Op_correcta)== 'Una instancia de la clase Class'
+					expect(@preg4.Op_incorrecta)==['una constante', 'un objeto', 'ninguna de las anteriores']
+				end
+				it "tiene que tener 3 componentes" do
+					expect{Preguntas::EleccionSimple.new(
+						 :pregunta => '5*8=?' )}.to raise_error(ArgumentError)
+				end
+# 				it " Además, se tendra una respuesta correcta única " do
+# 					expect(@preg5.Op_correcta) == 'Falso'
+# 				end
+			end
+			
+			
+			
  			context "Comprobacion de existencia de metodos" do
  				it "existe un metodo pop" do
  					expect(@lista_enlazada).to respond_to :pop
@@ -85,16 +102,6 @@ describe Prct06 do
 			  end
 			end
 
-
-
-
-
 		end 
-		
-	
-
 	end
-
-
- 
 end
