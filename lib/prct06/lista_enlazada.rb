0c2efc4a12a8ac356f57e1f402f062ff9b884065
Fabián Díaz Lorenzo <alu0100702545@ull.edu.es>
2014-11-05 19:20:33 +0000

    haciendo el push

--- lib/prct06/lista_enlazada.rb
+++ lib/prct06/lista_enlazada.rb
@@ -1,26 +1,61 @@
 module Prct06
   class Lista_enlazada
     
-    attr_accessor :cabeza, :ultelem,:valor, :siguiente
+    attr_accessor :cabeza,:valor, :siguiente,:antes
     
     # create a Struct with :value and :next
-    nodo = Struct.new(:valor, :siguiente)
+    nodo = Struct.new(:valor, :antes,:siguiente )
     
     def initialize(a=nil, b=nil)
    	@cabeza=a
 	@ultelem=b
     end
     
-    def push(args)
+    def push_antes(base,nuevo)
       if @cabeza== nil 
 	cabeza=args
       else
-	args.siguiente = cabeza
-	cabeza = args
+	
+	aux=@cabeza
+	while aux.valor =! base.valor 
+	 aux.siguiente = aux
+		
+	end  
+	 aux2=aux.antes
+	 aux.anterior=nuevo.siguiente
+	 aux2.siguiente=nuevo
+	 nuevo.siguiente=aux
+	 nuevo.anterior=aux2
+      end
+    end 
+     def push_despues(base,nuevo)
+      if @cabeza== nil 
+	cabeza=args
+      else
+	  aux=@cabeza
+	while aux.valor =! base.valor 
+	 aux.siguiente = aux
+	end  
+	 aux2=aux.siguiente
+	 aux.siguiente=nuevo
+	 nuevo.siguiente=aux2
+	 aux2.anterior=nuevo
+	 nuevo.anterior=aux
       end
     end 
     
-    def pop()
+    def pop_antes(nodo)
+      if @cabeza!=nil
+	
+	destruir = cabeza
+	cabeza = cabeza.siguiente  
+	destruir.valor
+      else
+	puts 'No habia nada en la lista enlazada'
+	return nil
+      end
+    end
+     def pop_despues(nodo)
       if @cabeza!=nil
 	destruir = cabeza
 	cabeza = cabeza.siguiente  
@@ -35,8 +70,10 @@ module Prct06
     end
     
   end
-  #class Lista_enlazada_doble
-  
+    def push_ante
+    end
+    def pop_final
+    end
   end
 end
     
