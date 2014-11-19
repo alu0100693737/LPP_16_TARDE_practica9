eb8bf59ff47e5edd5278154a2dff06267ef318f2
Ivan Garcia <alu0100693737@ull.edu.es>
2014-11-19 22:18:58 +0100

    ultimos cambios

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@
 
 
 require 'coveralls'
+Coveralls.wear!
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
@@ -19,5 +21,5 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = 'random'
 
-  Coveralls.wear!
+  
 end
