242d4547446c3ded0eda34e3ea0a3c7637fcc0f4
Fabián Díaz Lorenzo <alu0100702545@ull.edu.es>
2014-11-21 13:44:45 +0000

    arreglando la lista

--- lib/prct06/lista_enlazada.rb
+++ lib/prct06/lista_enlazada.rb
@@ -6,85 +6,32 @@ module Prct06
     
     # create a Struct with :value and :next
     nodo = Struct.new(:valor, :antes,:siguiente )
-    
-    def initialize(a=nil, b=nil)
-   	@cabeza=a
-	@ultelem=b
+    def initialize()
+   	@cabeza=nil
+	#@ultelem=b
     end
-    def push_antes(base,nuevo)
-      if @cabeza== nil 
-	@cabeza=nuevo
-      elsif base==nil 
-	nuevo.siguiente = @cabeza
-	@cabeza.antes = nuevo
-	#@cabeza= nuevo
-	#suponemos que nuevo anterio ya es nulo
-      else
-	aux=@cabeza
-	while aux.valor =! base.valor 
-	 aux.siguiente = aux
+ 
+    def push(preg)
+       if @cabeza ==nil
+	 @nodo1=nodo.new(preg,nil,nil)
+	 @cabeza=nodo
+	else
+	  @nodo1=nodo.new(preg,nil,nil)
+	  @cabeza.antes=@nodo1
+	  @nodo1.siguiente=@cabeza
+	  @cabeza=@nodo1
 	end  
-	#aux2=@cabeza
-	 aux2=aux.antes
-	 aux.antes=nuevo.siguiente
-	 aux2.siguiente = nuevo
-	 nuevo.siguiente = aux
-	 nuevo.antes = aux2
-      end
-    end 
-#     def push_despues(base,nuevo)
-#       if @cabeza== nil 
-# 	  @cabeza=nuevo
-#       elsif base==nil 
-# 	nuevo.siguiente = @cabeza
-# 	@cabeza.antes = nuevo
-# 	#suponemos que nuevo anterio ya es nulo
-#      else 
-# 	  aux=@cabeza
-# 	while aux.valor =! base.valor 
-# 	 aux.siguiente = aux
-# 	end  
-# 	 aux2=aux.siguiente
-# 	 aux.siguiente=nuevo
-# 	 nuevo.siguiente=aux2
-# 	 aux2.antes=nuevo
-# 	 nuevo.antes=aux
-#       end
-#     end 
-    def push()
-       
     end
     def pop()
+      if @cabeza== nil
+	puts "lista vacia"
+      else
+	node=@cabeza
+	@cabeza=@cabeza.siguiente
+	node.valor
+      end
     end
     
-    def pop(nodo)
-      if @cabeza!=nil
-	aux=@cabeza
-	while aux.valor =! nodo.valor 
-	 aux.siguiente = aux
-	end 
-	destruir =aux.valor
-	aux2=nodo.antes
-	aux3=nodo.siguiente
-	aux2.antes =aux3
-	aux3.antes=aux2
-	destruir.to_s
-      else
-	puts 'No habia nada en la lista enlazada'
-	return nil
-      end
-    end
-    def pop_principio()
-      if @cabeza !=null
-	aux=@cabeza
-	@cabeza=@cabeza.siguiente
-	@cabeza.antes=nil
-	aux.siguiente=nil
-	aux.valor
-      end
-    end  
-    def to_tex
-    end
     def empty
       if @cabeza==nil
 	a=true
@@ -104,3 +51,72 @@ module Prct06
   end
 end
     
+#     def push_antes(base,nuevo)
+#       if @cabeza== nil 
+# 	@cabeza=nuevo
+#       elsif base==nil 
+# 	nuevo.siguiente = @cabeza
+# 	@cabeza.antes = nuevo
+# 	#@cabeza= nuevo
+# 	#suponemos que nuevo anterio ya es nulo
+#       else
+# 	aux=@cabeza
+# 	while aux.valor =! base.valor 
+# 	 aux.siguiente = aux
+# 	end  
+# 	#aux2=@cabeza
+# 	 aux2=aux.antes
+# 	 aux.antes=nuevo.siguiente
+# 	 aux2.siguiente = nuevo
+# 	 nuevo.siguiente = aux
+# 	 nuevo.antes = aux2
+#       end
+#     end 
+#     def push_despues(base,nuevo)
+#       if @cabeza== nil 
+# 	  @cabeza=nuevo
+#       elsif base==nil 
+# 	nuevo.siguiente = @cabeza
+# 	@cabeza.antes = nuevo
+# 	#suponemos que nuevo anterio ya es nulo
+#      else 
+# 	  aux=@cabeza
+# 	while aux.valor =! base.valor 
+# 	 aux.siguiente = aux
+# 	end  
+# 	 aux2=aux.siguiente
+# 	 aux.siguiente=nuevo
+# 	 nuevo.siguiente=aux2
+# 	 aux2.antes=nuevo
+# 	 nuevo.antes=aux
+#       end
+#     end
+#   def pop(nodo)
+#       if @cabeza!=nil
+# 	aux=@cabeza
+# 	while aux.valor =! nodo.valor 
+# 	 aux.siguiente = aux
+# 	end 
+# 	destruir =aux.valor
+# 	aux2=nodo.antes
+# 	aux3=nodo.siguiente
+# 	aux2.antes =aux3
+# 	aux3.antes=aux2
+# 	destruir.to_s
+#       else
+# 	puts 'No habia nada en la lista enlazada'
+# 	return nil
+#       end
+#     end
+#     def pop_principio()
+#       if @cabeza !=null
+# 	aux=@cabeza
+# 	@cabeza=@cabeza.siguiente
+# 	@cabeza.antes=nil
+# 	aux.siguiente=nil
+# 	aux.valor
+#       end
+#     end  
+#     def to_tex
+
+
